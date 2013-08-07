b5fd5cbcc13a42bc52c5e2e7e305d2d8597f972f
Elmer Thomas <elmer.thomas@sendgrid.com>
2013-08-07 01:50:58 -0700

    Debugging charset issue

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,18 +1,5 @@
 SgEyRuby::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_charset = "utf-8"
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.smtp_settings = {
-       :authentication => :plain,
-       :address => "smtp.sendgrid.net",
-       :port => 587,
-       :domain => "thinkingserious.com",
-       :user_name => ENV["SENDGRID_USERNAME"],
-       :password => ENV["SENDGRID_PASSWORD"]
-  }
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
