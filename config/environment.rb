a6806b05184a96b626688984cb4dbfbe4be35813
Elmer Thomas <elmer.thomas@sendgrid.com>
2013-08-07 01:42:48 -0700

    Added default content type

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 SgEyRuby::Application.initialize!
+
+ActionMailer::Base.default :content_type => "text/html"
