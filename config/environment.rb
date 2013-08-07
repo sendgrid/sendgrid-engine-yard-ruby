ece2263a210b1b9102ef7ac061f617f958d2088d
Elmer Thomas <elmer.thomas@sendgrid.com>
2013-08-07 10:27:24 -0700

    Debugging SendGrid email config

--- config/environment.rb
+++ config/environment.rb
@@ -4,17 +4,20 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 SgEyRuby::Application.initialize!
 
-ActionMailer::Base.default :content_type => "text/html"
+SgEyRuby::Application.configure do
 
-config.action_mailer.delivery_method = :smtp
-# config.action_mailer.default_charset = "utf-8"
-config.action_mailer.perform_deliveries = true
-config.action_mailer.raise_delivery_errors = true
-config.action_mailer.smtp_settings = {
-     :authentication => :plain,
-     :address => "smtp.sendgrid.net",
-     :port => 587,
-     :domain => "sendgrid.com",
-     :user_name => ENV["SENDGRID_USERNAME"],
-     :password => ENV["SENDGRID_PASSWORD"]
-}
+  ActionMailer::Base.default :content_type => "text/html"
+
+  config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.default_charset = "utf-8"
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+       :authentication => :plain,
+       :address => "smtp.sendgrid.net",
+       :port => 587,
+       :domain => "sendgrid.com",
+       :user_name => ENV["SENDGRID_USERNAME"],
+       :password => ENV["SENDGRID_PASSWORD"]
+  }
+end
