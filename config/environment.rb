6719c658e584350ce44a44a63c83dfc5882e679b
Elmer Thomas <elmer.thomas@sendgrid.com>
2013-08-07 10:20:28 -0700

    Debugging SendGrid email config

--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,16 @@ require File.expand_path('../application', __FILE__)
 SgEyRuby::Application.initialize!
 
 ActionMailer::Base.default :content_type => "text/html"
+
+config.action_mailer.delivery_method = :smtp
+# config.action_mailer.default_charset = "utf-8"
+config.action_mailer.perform_deliveries = true
+config.action_mailer.raise_delivery_errors = true
+config.action_mailer.smtp_settings = {
+     :authentication => :plain,
+     :address => "smtp.sendgrid.net",
+     :port => 587,
+     :domain => "sendgrid.com",
+     :user_name => ENV["SENDGRID_USERNAME"],
+     :password => ENV["SENDGRID_PASSWORD"]
+}
