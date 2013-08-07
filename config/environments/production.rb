5a4bb291f9ad11e63a4f0662ff182a353f60312b
Elmer Thomas <elmer.thomas@sendgrid.com>
2013-08-07 01:29:13 -0700

    Fixing issue with charset

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 SgEyRuby::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  ActionMailer::Base.default :content_type => "text/html"
+  
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_charset = "utf-8"
   config.action_mailer.perform_deliveries = true
