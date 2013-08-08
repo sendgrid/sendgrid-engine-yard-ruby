d2128ce658a9409fba1d505afdbf296561f0a8be
Elmer Thomas <elmer.thomas@sendgrid.com>
2013-08-08 13:40:16 -0700

    Changed method of URL contruction

--- app/mailers/emailer.rb
+++ app/mailers/emailer.rb
@@ -1,10 +1,9 @@
 class Emailer < ActionMailer::Base
   default :from => "elmer.thomas@sendgrid.com"
-  domain = "http://yourdomain.com"
   
   def send_email(email)  
     # Contruct the URL to where the email is stored on the web
-    @url = domain + "/" + "?read=" + email.id.to_s
+    @url = "http://yourdomain.com?read=" + email.id.to_s
     subject = "re: " + email.from
     mail(:to => email.from, :subject => subject)
   end
